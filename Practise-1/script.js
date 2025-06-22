6da4726e9f92478aaef3687442312e0a7a1f0f96
Aparnaraha <aparnaraha2912@gmail.com>
2025-06-22 22:48:56 +0530

    Practise-updated

--- Practise-1/script.js
+++ Practise-1/script.js
@@ -1,3 +1,5 @@
-console.log("hii")
-
-document.querySelector('h1').innerHTML = "helloworld"
+console.log("Hello, Node.js World!");
+console.log("Directory:", __dirname);
+console.log("File:", __filename);
+console.log("Platform:", process.platform);
+console.log("Node Version:", process.version);
